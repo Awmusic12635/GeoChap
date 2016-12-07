41be252950c713c0362fcb344c62a070d4ccca9f
Alex_Wacker <alex@alexwacker.com>
2016-12-07 17:14:56 -0500

    Perhaps this will fix it

--- resources/views/admin/caches.blade.php
+++ resources/views/admin/caches.blade.php
@@ -1,33 +1,36 @@
 @extends('layouts.admin')
 
 @section('content')
-<table id="cachetable">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Lat</th>
-            <th>Long</th>
-            <th>Status</th>
-            <th>Size</th>
-            <th>Type</th>
-            <th>Approved</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($caches as $cache)
-        <tr>
-            <td>{{$cache->name}}</td>
-            <td>{{$cache->lat}}</td>
-            <td>{{$cache->long}}</td>
-            <td>{{$cache->status}}</td>
-            <td>{{$cache->size}}</td>
-            <td>{{$cache->type}}</td>
-            <td>{{$cache->approved}}</td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
-<script>
-    $('#cachetable').DataTable();
-</script>
+<!-- page content -->
+<div class="right_col" role="main">
+    <table id="cachetable">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Lat</th>
+                <th>Long</th>
+                <th>Status</th>
+                <th>Size</th>
+                <th>Type</th>
+                <th>Approved</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($caches as $cache)
+            <tr>
+                <td>{{$cache->name}}</td>
+                <td>{{$cache->lat}}</td>
+                <td>{{$cache->long}}</td>
+                <td>{{$cache->status}}</td>
+                <td>{{$cache->size}}</td>
+                <td>{{$cache->type}}</td>
+                <td>{{$cache->approved}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    <script>
+        $('#cachetable').DataTable();
+    </script>
+</div>
 @endsection
