aa3565821bbcdd61eaedb411a975294c6e34aac2
Alex_Wacker <alex@alexwacker.com>
2016-12-07 19:06:43 -0500

    attempt to make it facier

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,7 +64,7 @@ class AdminController extends Controller
      */
 
     public function addCache(Request $request){
-        
+
     }
 
 }
--- resources/views/admin/newcache.blade.php
+++ resources/views/admin/newcache.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.admin')
 
 @section('content')
-<form class="form-horizontal" role="form" method="POST" action="{{ url('/admin/caches') }}">
+<div class="page-title">
+    <div class="title_left">
+        <h3>Add New Cache</h3>
+    </div>
+</div>
+<div class="clearfix"></div>
+<div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Add New Cache Form</h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+                <br />
+<form class="form-horizontal form-label-left" role="form" method="POST" action="{{ url('/admin/caches') }}">
     {{ csrf_field() }}
     <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
         <label for="name" class="col-md-4 control-label">Name</label>
@@ -81,4 +100,8 @@
         </div>
     </div>
 </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
