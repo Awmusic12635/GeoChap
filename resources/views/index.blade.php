06f1ef397ac684c1c40edf52de6654336ae902dd
Alex_Wacker <alex@alexwacker.com>
2016-12-08 22:51:09 -0500

    Lets try to get rid of the random brackets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <div class="clearfix"></div>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
-            <div style="height:350px;width:100%;" id="map">{{!! Mapper::render(); !!}}</div>
+            <div style="height:350px;width:100%;" id="map">{!! Mapper::render() !!}</div>
             <div class="x_panel">
 
             </div>
