b692db06cd0f1ab8544b91d421e5e795b5647a06
Alex_Wacker <alex@alexwacker.com>
2016-12-08 20:04:45 -0500

    lets try giving it a size

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,13 @@
         </div>
     </div>
     <div class="clearfix"></div>
-    <div id="map"></div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="x_panel">
+                <div id="map"></div>
+            </div>
+        </div>
+    </div>
     <script>
         var map;
         function initMap() {
