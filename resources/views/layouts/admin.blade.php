e8bd29c7be59240eed4659f18f68030eecff74cb
Valtarre <l337sniper7@hotmail.com>
2016-12-09 12:13:03 -0500

    switched datatables link to locally hosted file

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -189,7 +189,7 @@
 
 <!-- jQuery -->
 <script src="/vendors/jquery/dist/jquery.min.js"></script>
-<script src="cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js"></script>
+<script src="/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
 <!-- Bootstrap -->
 <script src="/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
