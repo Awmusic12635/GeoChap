a059a72a3c988e1de67aaf86ccc8050f4d0334ed
Alex_Wacker <alex@alexwacker.com>
2016-12-08 20:06:28 -0500

    lets try giving it a size

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <div class="clearfix"></div>
     <div class="row">
         <div class="col-md-12 col-sm-12 col-xs-12">
-            <div id="map"></div>
+            <div style="height:200px;width:500px;" id="map"></div>
             <div class="x_panel">
 
             </div>
